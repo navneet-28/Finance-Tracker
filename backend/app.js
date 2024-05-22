f4e2c5a03010bb7237c5e6a939bac54be4d5dee4
navneet-28 <snavneet20@iitk.ac.in>
2024-05-22 16:16:03 +0530

    modified backend

--- backend/app.js
+++ backend/app.js
@@ -7,16 +7,26 @@ const userRoutes = require('./routes/userRoutes');
 
 require('dotenv').config();
 
-const PORT = process.env.BACKEND_PORT || 5000;
+const PORT = process.env.BACKEND_PORT || 8080;
 
 
 const corsOptions = {
-    origin: 'http://localhost:3000', // Specify the client origin
+    origin: 'https://finance-tracker-chi-two.vercel.app', // Specify the client origin
     credentials: true, // Enable cookies and other credentials
   };
 
 app.use(express.json());
 app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+    res.setHeader('Referrer-Policy', 'strict-origin-when-cross-origin');
+    next();
+  });
+
+  app.use((req, res, next) => {
+    res.setHeader("Permissions-Policy", "geolocation=(self), microphone=()");
+  next();
+  });
 
 //routes
 readdirSync('./routes').map((r)=> app.use('/api', require(`./routes/${r}`)));
